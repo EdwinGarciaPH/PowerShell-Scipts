e641a40e783b54aaf5005fcad910bdf60e0d7f43
Edwin Santos Garcia <Edwin_GarciaPH@outlook.com>
2019-08-10 10:36:14 +0700

    Update GUIDemo4.ps1

--- GUIDemo4.ps1
+++ GUIDemo4.ps1
@@ -4,13 +4,17 @@
 $Form = New-Object System.Windows.Forms.Form    
 $Form.Size = New-Object System.Drawing.Size(600,400)  
 
+$Form.StartPosition = "CenterScreen" #loads the window in the center of the screen
+$Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedToolWindow #modifies the window border
+$Form.Text = "Ping GUI tool" #window description
+
 ############################################## Start functions
 
 function pingInfo {
 $wks=$InputBox.text;
 $pingResult=ping $wks | fl | out-string;
 $outputBox.text=$pingResult
-                     } #end pingInfo
+} #end pingInfo
 
 ############################################## end functions
 
@@ -26,6 +30,8 @@ $outputBox.Location = New-Object System.Drawing.Size(10,150)
 $outputBox.Size = New-Object System.Drawing.Size(565,200) 
 $outputBox.MultiLine = $True 
 $outputBox.ScrollBars = "Vertical" 
+
+$outputBox.Font = New-Object System.Drawing.Font("Verdana",8,[System.Drawing.FontStyle]::Italic)
 $Form.Controls.Add($outputBox) 
 
 ############################################## end text fields
@@ -37,6 +43,11 @@ $Button.Location = New-Object System.Drawing.Size(400,30)
 $Button.Size = New-Object System.Drawing.Size(110,80) 
 $Button.Text = "Ping" 
 $Button.Add_Click({pingInfo}) 
+
+$Button.Cursor = [System.Windows.Forms.Cursors]::Hand
+$Button.BackColor = [System.Drawing.Color]::LightGreen
+$Button.Font = New-Object System.Drawing.Font("Verdana",14,[System.Drawing.FontStyle]::Bold)
+
 $Form.Controls.Add($Button) 
 
 ############################################## end buttons
